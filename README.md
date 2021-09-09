7f445735eb5311f7a8fc3e8b48548808b11aec11
Gagarin <fobrales@gmail.com>
2021-09-09 16:04:19 +0300

    add readme, fixed bug with desc sorting, correct text display in about column

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# table
+# Table
+
+This project is test task.
+
+### Sort entries by field
+
+Click on the required column in the table header. The table will be sorted by this column in ascending order. Click again to sort in descending order. Sorting by color is also available.
+
+### Hide/show column
+
+At the top of the page, uncheck/сheck the boxes on the column name. 
+
+### Set page
+
+Select the required page from the drop-down list at the top of the page. 
+
+### Edit entries
+
+Click on the table row you want to edit. A form for editing will appear on the right. Change the fields and click the "Edit" button. If you don't want to edit the row, click the "Cancel" button.
--- js/table.js
+++ js/table.js
@@ -1,6 +1,7 @@
-var allHeaders = ['ID', 'First name', 'Last name', 'Phone', 'About', 'Eye color'];
+var allHeaders = ['First name', 'Last name', 'Phone', 'About', 'Eye color'];
 var globalData = [];
 var globalPage = 1;
+var sorted = '';
 
 // getting data from JSON file.
 function getData(file, callback) {
@@ -70,45 +71,74 @@ function makeColorCircle(color) {
 //  for field "name" make other column for last name,
 //  for field "eyeColor" make color providing
 function makeField(field, string, data) {
-    var column = document.createElement('td');
-    string.append(column);
-    switch (field) {
-        case 'name':
-            var lastName = data.name.lastName;
-            makeLastName(lastName, string);
-            column.innerHTML = data.name.firstName;
-            column.className = 'firstname';
-            break;
-        case 'eyeColor':
-            column.style.backgroundColor = data.eyeColor;
-            column.className = field.toLowerCase();
-            break;
-        default:
-            column.className = field.toLowerCase();
-            column.innerHTML = data[field];
+    if (field != 'id') {
+        var column = document.createElement('td');
+        string.append(column);
+        switch (field) {
+            case 'name':
+                var lastName = data.name.lastName;
+                makeLastName(lastName, string);
+                column.innerHTML = data.name.firstName;
+                column.className = 'firstname';
+                break;
+            case 'eyeColor':
+                column.style.backgroundColor = data.eyeColor;
+                column.className = field.toLowerCase();
+                break;
+            default:
+                column.className = field.toLowerCase();
+                column.innerHTML = data[field];
+        }
     }
 }
 
+// sort object array by concrete object property.
 function sortByField() {
     let sortField = this.innerText.replace(' ', '').toLowerCase();
-    let entries = document.getElementsByClassName(sortField);
-    console.log(entries);
+    let newData = [];
+    let reverse = false;
+    if (sorted == 'asc') {
+        reverse = true;
+    }
+    switch (sortField) {
+        case "lastname":
+            newData = globalData.sort((a, b) => (a.name.lastName.toLowerCase() < b.name.lastName.toLowerCase() ? -1 : 1));
+            break;
+        case "firstname":
+            newData = globalData.sort((a, b) => (a.name.firstName.toLowerCase() < b.name.firstName.toLowerCase() ? -1 : 1));;
+            break;
+        case "eyecolor":
+            newData = globalData.sort((a, b) => a.eyeColor < b.eyeColor ? -1 : 1);
+            break;
+        default:
+            newData = globalData.sort((a, b) => a[sortField].toLowerCase() < b[sortField].toLowerCase() ? -1 : 1);
+            break;
+    }
+    if (reverse) {
+        makeTable(newData.reverse());
+        sorted = 'desc';
+    } else {
+        makeTable(newData);
+        sorted = 'asc';
+    }
 }
 
+// hide concrete column by using "display: none" style property;
 function hideColumn() {
     let field = this.name.replace(' ', '').toLowerCase();
     let columns = document.body.getElementsByClassName(field);
     if (this.checked) {
         for (let field of columns) {
-            field.style = '';
+            field.style.display = '';
         }
     } else {
         for (let field of columns) {
-            field.style = 'display: none';
+            field.style.display = 'none';
         }
     }
 }
 
+// create form for editing string.
 function editStringForm() {
     const form = document.getElementById('edit_string');
     form.innerHTML = '';
@@ -135,6 +165,7 @@ function makeEditingInput(attr, form, string) {
     form.append(cont);
 }
 
+// make button panel for editing and bind function editString()
 function makeEditingButtons(form, string) {
     cont = document.createElement('div');
     cont.className = 'func-editing-buttons';
@@ -152,11 +183,12 @@ function makeEditingButtons(form, string) {
     return cont;
 }
 
-
+// editing entry in table; if value in table cell different from new value in editing form, value in cell and global data update after click on "edit".
 function editString() {
     const fields = document.getElementsByClassName('form-input-editing');
     findString = this.name;
     let entry = document.getElementById(findString);
+    let id = entry.id;
     for (let field of fields) {
         let attr = field.firstChild.textContent.replace(' ', '').toLowerCase();
         let column = entry.querySelector("." + attr);
@@ -165,13 +197,37 @@ function editString() {
             switch (column.className) {
                 case 'eyecolor':
                     column.style.backgroundColor = newValue;
+                    editGlobalData(column.className, id, newValue);
                     break;
                 default:
+                    editGlobalData(column.className, id, newValue);
                     column.innerText = newValue;
             }
         }
     }
 }
+
+// functon for editing global data
+function editGlobalData(column, id, newValue) {
+    let field = column;
+    switch (column) {
+        case 'firstname':
+            field = 'firstName';
+            break;
+        case 'lastname':
+            field = 'lastName';
+            break;
+        case 'eyecolor':
+            field = 'eyeColor';
+            break;
+    }
+    for (let entry of globalData) {
+        if (entry.id == id) {
+            entry[field] = newValue;
+        }
+    }
+}
+
 // make table header with function sorting on click concrete header
 function makeHeadTable(table) {
     var head = document.createElement('tr');
@@ -186,6 +242,7 @@ function makeHeadTable(table) {
     table.append(head);
 }
 
+// make panel with checkboxes for hide/show the columns
 function makePanelForHide() {
     let panel = document.createElement('div');
     panel.className = 'hide-panel';
@@ -208,11 +265,13 @@ function makePanelForHide() {
     document.body.append(panel);
 }
 
+// set value of globalPage and re-create the table with data from page.
 function setPage() {
     globalPage = this.value;
     makeTable(globalData, globalPage);
 }
 
+// make select element for changing page
 function makePageSelector(data) {
     let panel = document.createElement('div');
     panel.append(document.createTextNode('Show page: '));
@@ -229,22 +288,25 @@ function makePageSelector(data) {
     document.body.append(panel);
 }
 
+// function for data array fragmentation into some amount subarrays
 function pagination(input, size) {
     return input.reduce((arr, item, i) => {
         return (i % size === 0 ? [...arr, [item]] : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]]);
     }, []);
 }
 
-function getDataFromPage(page) {
-    let pages = Math.ceil(globalData.length / 10);
-    let paginated = pagination(globalData, 10);
+// paging function call and get data from concrete page
+function getDataFromPage(page, data) {
+    let pages = Math.ceil(data.length / 10);
+    let paginated = pagination(data, 10);
     return paginated[page-1];
 }
 
+// make table from data on concrete page
 function makeTable(data = globalData, page = globalPage) {
     let tableContainer = document.getElementById('table_container');
     tableContainer.innerHTML = '';
-    data = getDataFromPage(page);
+    data = getDataFromPage(page, data);
     var table = document.createElement('table');
     tableContainer.append(table);
     makeHeadTable(table);
